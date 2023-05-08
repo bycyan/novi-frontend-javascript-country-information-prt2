ddbf0a9be5f2f738c3427730f4253b95188d873d
Cyan Dalebout <cyan@wervelwindmedia.nl>
2023-05-08 16:49:36 +0200

    some edits + bonus

--- src/app.js
+++ src/app.js
@@ -1,37 +1,27 @@
 import axios from 'axios';
 
-// sla de referentie naar het formulier op en plaats er een submit-event listener op
 const searchForm = document.getElementById('search-form');
 searchForm.addEventListener('submit', searchCountry);
 
-// sla de referentie op naar het error-element en het zoek-resultaat-element
 const countryInfoBox = document.getElementById('search-result');
 const errorMessageBox = document.getElementById('error-message');
 
 function searchCountry(e) {
-    // zorg ervoor dat de pagina niet ververst
     e.preventDefault();
-    // sla de referentie naar het invoerveld op
-    const queryfield = document.getElementById('query-field');
-    // roep de fetchCountryDetails functie aan en geef de zoekterm mee
-    fetchCountryDetails(queryfield.value);
-    // maak het invoerveld weer leeg!
-    queryfield.value = '';
+    const queryField = document.getElementById('query-field');
+    fetchCountryDetails(queryField.value);
+    queryField.value = '';
 }
 
 async function fetchCountryDetails(name) {
-    // zorg ervoor dat er iedere keer als er een nieuwe zoekopdracht gedaan wordt, het (mogelijke) oude resultaat
-    // en (mogelijke) oude error-message worden verwijderd
     countryInfoBox.innerHTML = ``;
     errorMessageBox.innerHTML = ``;
 
     try {
-        // probeer de gegevens over dit land op te halen
         const result = await axios.get(`https://restcountries.com/v2/name/${name}`);
         const country = result.data[0];
         console.log(country);
 
-        // vul de countryInfoBox met de volgende html-elementen:
         countryInfoBox.innerHTML = `
       <article class="search-result-box">
         <span class="flag-title-container">
@@ -40,18 +30,17 @@ async function fetchCountryDetails(name) {
         </span>
         <p>${country.name} is situated in ${country.subregion}. It has a population of ${country.population.toLocaleString()} people</p>
         <p>The capital is ${country.capital} ${createCurrencyDescription(country.currencies)}</p>
+        <p>${createLanguageDescription(country.languages)}</p>
       </article>
     `;
     } catch (e) {
         console.error(e);
-        // is er iets misgegaan? Vul dan de error-message box met de volgende elementen:
         errorMessageBox.innerHTML = `
       <p class="error-message">${name} doesn't exist. Please try again.</p>
     `;
     }
 }
 
-// deze functie kan iedere keer opnieuw aangeroepen worden om een valuta-string te genereren
 function createCurrencyDescription(currencies) {
     let output = 'and you can pay with ';
 
@@ -60,4 +49,16 @@ function createCurrencyDescription(currencies) {
     }
 
     return output + `${currencies[0].name}'s`;
+}
+
+function createLanguageDescription(languages){
+    let output = 'They speak ';
+
+    if(languages.length === 2){
+        return output + `${languages[0].name} and ${languages[1].name}`;
+    } else if(languages.length === 3){
+        return output + `${languages[0].name}, ${languages[1].name} and ${languages[2].name}`;
+    }
+
+    return output + `${languages[0].name}`;
 }
